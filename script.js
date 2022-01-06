03a65990e658419e4417332b3a4037dcc1861197
Allan Acosta <acosta.gov@gmail.com>
2022-01-06 16:20:48 -0500

    rid of navbar, added overlay

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ const nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150){
+    if (window.scrollY > nav.offsetHeight + 150) {
         nav.classList.add('active')
     } else {
         nav.classList.remove('active')
     }
 }
 
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
